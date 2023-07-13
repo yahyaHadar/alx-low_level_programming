c1479fb65c4811cc3618198307cb87cc4a8e771e
yahyaHadar <yahyahadar@gmail.com>
2023-07-13 11:07:39 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -6,12 +5,11 @@
  *
  * Description: A C program that prints a line to the standard error
  *
- * Return: Always 1 (Success)
- *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;
+    return (1);
 }
