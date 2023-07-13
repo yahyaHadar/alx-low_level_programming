400b686cf2a96e3c3320a43a61b611356eaf547e
yahyaHadar <yahyahadar@gmail.com>
2023-07-13 11:23:41 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,11 @@
 /**
  * main - Entry point of the program
  *
- * Description: A C program that prints a line to the standard error
- *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
