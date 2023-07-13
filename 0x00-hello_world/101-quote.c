c7ae8269eedb3b663f6e51d7c816513f90b538f4
yahyaHadar <yahyahadar@gmail.com>
2023-07-13 10:56:21 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
  *
  * Description: A C program that prints a line to the standard error
  *
- * Return: Always 1 (Success)
+ * Return: Always 1 (erro)
  */
+
 int main(void)
 {
 	char quo[] = "and that place of art is useful\" - Dora Korpar, 2015-10-19\n";
-
 	write(1, quo, 59);
 	return (1);
 }
