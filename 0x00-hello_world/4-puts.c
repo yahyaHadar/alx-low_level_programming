c30a0376f775c2948b4854b47ae229e7299dc92d
yahyaHadar <yahyahadar@gmail.com>
2023-07-13 05:42:35 -0700

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Description: A C program that prints with put function
+ *
+ *retuen: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
