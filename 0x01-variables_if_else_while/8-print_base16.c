9ebebb5a41fd6fd35673022c562c07765eee33eb
yahyaHadar <yahyahadar@gmail.com>
2023-07-14 16:33:58 -0700

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry mode
+ *
+ * Descreption: print numbers of base 16
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
