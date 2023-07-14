057a75895997aec1d4415103ddb7de5fe9acfa7a
yahyaHadar <yahyahadar@gmail.com>
2023-07-14 16:23:41 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: prnt alphabets in reverse
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
