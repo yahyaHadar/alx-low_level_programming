e752d1734dc3cc5698a4b205a6584176e00c48aa
yahyaHadar <yahyahadar@gmail.com>
2023-08-07 10:21:46 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,6 +33,12 @@ char *str_concat(char *s1, char *s2)
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
+		i++;
+	}
+
+	while (s2[c] != '\0')
+	{
+		conct[i] = s2[c];
 		i++, c++;
 	}
 	conct[i] = '\0';
