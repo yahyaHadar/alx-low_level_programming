c3f43361402f81172ae41e3b0923c6db6366e37a
yahyaHadar <yahyahadar@gmail.com>
2023-08-08 06:59:51 -0700

    m

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,14 +11,14 @@
 
 int count_word(char *s)
 {
-	int flag, c, w;
+	int flag, e, w;
 
 	flag = 0;
 	w = 0;
 
-	for (c = 0; s[c] != '\0'; c++)
+	for (e = 0; s[e] != '\0'; e++)
 	{
-		if (s[c] == ' ')
+		if (s[e] == ' ')
 			flag = 0;
 		else if (flag == 0)
 		{
