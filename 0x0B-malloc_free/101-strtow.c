bd71dc2ca984b5d1e2b003dd99e4361c4a0b121d
yahyaHadar <yahyahadar@gmail.com>
2023-08-08 07:25:58 -0700

    m

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -39,7 +39,7 @@ int count_word(char *s)
 char **strtow(char *str)
 {
 	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start, end;
+	int i, k = 0, len = 0, words, c = 0, start = 0, end;
 
 	while (*(str + len))
 		len++;
