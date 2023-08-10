35bacdbc5c44fcd3f1972a48e5bb354e2ef70a76
yahyaHadar <yahyahadar@gmail.com>
2023-08-10 07:53:39 -0700

    commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,13 +20,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size == old_size)
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
 	if (!ptr)
-		return (malloc(new_size));
+	i	return (malloc(new_size));
 
 	ptr1 = malloc(new_size);
 	if (!ptr)
@@ -39,6 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		for (i = 0; i < new_size; i++)
 			ptr1[i] = old_ptr[i];
 	}
+
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
