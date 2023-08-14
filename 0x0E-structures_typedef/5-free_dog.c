abead648e305b10a074a9810d49e69adf0287300
yahyaHadar <yahyahadar@gmail.com>
2023-08-14 09:41:28 -0700

    commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,21 +2,17 @@
 #include "dog.h"
 
 /**
- * free_dog - frees memmory allocated for a struct dog
+ * free_dog - frees memory allocated for a struct dog
  *
- * @d: struct dog to go free
- *
- * Return: void.
+ * @d: struct dog to free
 */
 
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		if (d->name)
-			free(d->name);
-		if (d->owner)
-			free(d->owner);
+		free(d->name);
+		free(d->owner);
 		free(d);
 	}
 }
