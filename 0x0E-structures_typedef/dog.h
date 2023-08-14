7ceae8d96c94356f542238136c96a2a09f76dcf9
yahyaHadar <yahyahadar@gmail.com>
2023-08-14 08:34:43 -0700

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ *
+ * @name: First member
+ * @age: Second mmember
+ * @owner: third memeber
+ *
+ * Description: Longer description
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
