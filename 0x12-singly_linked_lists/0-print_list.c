5563e0991fe248f6681b70fe36b67546bc874d10
yahyaHadar <yahyahadar@gmail.com>
2023-08-24 06:57:23 -0700

    first commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ *
+ * @s: the string whose lenght to check
+ *
+ * Return: integer leght of string
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - prints a linked lists
+ *
+ * @h: pointer to first node
+ *
+ * Return: size of list
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
