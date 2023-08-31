ee1a6f8d19a00b4f220078de06c9da7f27287c7d
yahyaHadar <yahyahadar@gmail.com>
2023-08-31 16:07:48 -0700

    commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit 
+ *
+ * @n: the number to index
+ * @index: bit to get
+ *
+ * Return: value of bit
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return (n >> inedx & 1);
+}
