149a3e05d6c733a8b7be05ebbe614aee65c03baa
yahyaHadar <yahyahadar@gmail.com>
2023-09-04 09:35:12 -0700

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -63,7 +63,7 @@ void close_file(int fb)
 
 int main(int argc, char *argv[])
 {
-	int start, fr, a, b;
+	int from, to, r, w;
 	char *buffer;
 
 	if (argc != 3)
@@ -73,36 +73,36 @@ int main(int argc, char *argv[])
 	}
 
 	buffer = create_buffer(argv[2]);
-	start = open(argv[1], O_RDONLY);
-	a = read(start, buffer, 1024);
-	fr = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (start == -1 || a == -1)
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv[1]);
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
 
-		b = write(fr, buffer, a);
-		if (fr == -1 || b == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't write to file %s\n", argv[2]);
+				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
 
-		a = read(start, buffer, 1024);
-		fr = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (a > 0);
+	} while (r > 0);
 
 	free(buffer);
-	close_file(start);
-	close_file(fr);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
