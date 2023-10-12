c7a861d3f214cbd39b70cf5e60765201d97a75b3
yahyaHadar <yahyahadar@gmail.com>
2023-10-12 14:34:26 +0100

    commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints doubly-linked lists
- * 
- * @h: the address of head node
- * 
- * Return: size of list
+ * print_dlistint - prints all the elemnts
+ *
+ * @h: head of the list
+ *
+ * Return: number of the nodes
 */
 
 size_t print_dlistint(const dlistint_t *h)
