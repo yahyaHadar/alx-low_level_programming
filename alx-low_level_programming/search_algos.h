96b5ee633dbacc72a801827952d2995cd819c860
yahyaHadar <yahyahadar@gmail.com>
2024-02-06 07:25:46 -0800

    commit

---
+++ alx-low_level_programming/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_search_help(int *array, int value, int left_index, int right_index);
+void printArray(int *array, int left_index, int right_index);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+
+#endif
